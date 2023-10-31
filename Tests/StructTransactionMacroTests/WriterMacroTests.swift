b27e2458668a4f78cd94b7e4c9597a9d7fc9007f
Hiroshi Kimura <muukii.app@gmail.com>
2023-10-31 13:21:51 +0900

    Use swift-macro-testing (#3)

--- Tests/StructTransactionMacroTests/WriterMacroTests.swift
+++ Tests/StructTransactionMacroTests/WriterMacroTests.swift
@@ -1,14 +1,24 @@
 import SwiftSyntaxMacros
 import SwiftSyntaxMacrosTestSupport
 import XCTest
+import MacroTesting
 
 import StructTransactionMacros
 
 final class WriterMacroTests: XCTestCase {
 
+  override func invokeTest() {
+    withMacroTesting(
+      isRecording: false,
+      macros: ["Detecting": WriterMacro.self]
+    ) {
+      super.invokeTest()
+    }
+  }
+
   func test_() {
 
-    assertMacroExpansion(
+    assertMacro {
       #"""
       @Detecting
       struct MyState {
@@ -17,78 +27,78 @@ final class WriterMacroTests: XCTestCase {
         var stored_property_wrapper: String = ""
 
       }
-      """#,
-      expandedSource: #"""
-struct MyState {
+      """#
+    } expansion: {
+      """
+      struct MyState {
 
-  @MyPropertyWrapper
-  var stored_property_wrapper: String = ""
+        @MyPropertyWrapper
+        var stored_property_wrapper: String = ""
 
-}
+      }
 
-extension MyState: DetectingType {
+      extension MyState: DetectingType {
 
-  typealias ModifyingTarget = Self
+        typealias ModifyingTarget = Self
 
-  @discardableResult
-  public static func modify(source: inout Self, modifier: (inout Modifying) throws -> Void) rethrows -> ModifyingResult {
+        @discardableResult
+        public static func modify(source: inout Self, modifier: (inout Modifying) throws -> Void) rethrows -> ModifyingResult {
 
-    try withUnsafeMutablePointer(to: &source) { pointer in
-      var modifying = Modifying(pointer: pointer)
-      try modifier(&modifying)
-      return ModifyingResult(
-        readIdentifiers: modifying.$_readIdentifiers,
-        modifiedIdentifiers: modifying.$_modifiedIdentifiers
-      )
+          try withUnsafeMutablePointer(to: &source) { pointer in
+            var modifying = Modifying(pointer: pointer)
+            try modifier(&modifying)
+            return ModifyingResult(
+              readIdentifiers: modifying.$_readIdentifiers,
+              modifiedIdentifiers: modifying.$_modifiedIdentifiers
+            )
+          }
+        }
+
+        @discardableResult
+        public static func read(source: Self, reader: (Modifying) throws -> Void) rethrows -> ReadResult {
+          // FIXME: avoid copying
+          var reading = source
+
+          return try withUnsafeMutablePointer(to: &reading) { pointer in
+            let modifying = Modifying(pointer: pointer)
+            try reader(modifying)
+            return ReadResult(
+              readIdentifiers: modifying.$_readIdentifiers
+            )
+          }
+        }
+
+        public struct Modifying /* want to be ~Copyable */ {
+
+          public private (set) var $_readIdentifiers: Set<String> = .init()
+          public private (set) var $_modifiedIdentifiers: Set<String> = .init()
+
+          private let pointer: UnsafeMutablePointer<ModifyingTarget>
+
+          init(pointer: UnsafeMutablePointer<ModifyingTarget>) {
+            self.pointer = pointer
+          }
+
+          public var stored_property_wrapper: String  {
+          mutating _read {
+            $_readIdentifiers.insert("stored_property_wrapper")
+            yield pointer.pointee.stored_property_wrapper
+          }
+          _modify {
+            $_modifiedIdentifiers.insert("stored_property_wrapper")
+            yield &pointer.pointee.stored_property_wrapper
+          }
+        }
+        }
+      }
+      """
     }
-  }
-
-  @discardableResult
-  public static func read(source: Self, reader: (Modifying) throws -> Void) rethrows -> ReadResult {
-    // FIXME: avoid copying
-    var reading = source
-
-    return try withUnsafeMutablePointer(to: &reading) { pointer in
-      let modifying = Modifying(pointer: pointer)
-      try reader(modifying)
-      return ReadResult(
-        readIdentifiers: modifying.$_readIdentifiers
-      )
-    }
-  }
-
-  public struct Modifying /* want to be ~Copyable */ {
-
-    public private (set) var $_readIdentifiers: Set<String> = .init()
-    public private (set) var $_modifiedIdentifiers: Set<String> = .init()
-
-    private let pointer: UnsafeMutablePointer<ModifyingTarget>
-
-    init(pointer: UnsafeMutablePointer<ModifyingTarget>) {
-      self.pointer = pointer
-    }
-
-    public var stored_property_wrapper: String  {
-    mutating _read {
-      $_readIdentifiers.insert("stored_property_wrapper")
-      yield pointer.pointee.stored_property_wrapper
-    }
-    _modify {
-      $_modifiedIdentifiers.insert("stored_property_wrapper")
-      yield &pointer.pointee.stored_property_wrapper
-    }
-  }
-  }
-}
-"""#,
-      macros: ["Detecting": WriterMacro.self]
-    )
 
   }
 
   func test_1() {
 
-    assertMacroExpansion(
+    assertMacro {
       #"""
       @Detecting
       struct MyState {
@@ -96,78 +106,102 @@ extension MyState: DetectingType {
         let constant_has_initial_value: Int = 0
 
       }
-      """#,
-      expandedSource: #"""
-struct MyState {
+      """#
+    } expansion: {
+      """
+      struct MyState {
 
-  let constant_has_initial_value: Int = 0
+        let constant_has_initial_value: Int = 0
 
-}
+      }
 
-extension MyState: DetectingType {
+      extension MyState: DetectingType {
 
-  typealias ModifyingTarget = Self
+        typealias ModifyingTarget = Self
 
-  @discardableResult
-  public static func modify(source: inout Self, modifier: (inout Modifying) throws -> Void) rethrows -> ModifyingResult {
+        @discardableResult
+        public static func modify(source: inout Self, modifier: (inout Modifying) throws -> Void) rethrows -> ModifyingResult {
 
-    try withUnsafeMutablePointer(to: &source) { pointer in
-      var modifying = Modifying(pointer: pointer)
-      try modifier(&modifying)
-      return ModifyingResult(
-        readIdentifiers: modifying.$_readIdentifiers,
-        modifiedIdentifiers: modifying.$_modifiedIdentifiers
-      )
+          try withUnsafeMutablePointer(to: &source) { pointer in
+            var modifying = Modifying(pointer: pointer)
+            try modifier(&modifying)
+            return ModifyingResult(
+              readIdentifiers: modifying.$_readIdentifiers,
+              modifiedIdentifiers: modifying.$_modifiedIdentifiers
+            )
+          }
+        }
+
+        @discardableResult
+        public static func read(source: Self, reader: (Modifying) throws -> Void) rethrows -> ReadResult {
+          // FIXME: avoid copying
+          var reading = source
+
+          return try withUnsafeMutablePointer(to: &reading) { pointer in
+            let modifying = Modifying(pointer: pointer)
+            try reader(modifying)
+            return ReadResult(
+              readIdentifiers: modifying.$_readIdentifiers
+            )
+          }
+        }
+
+        public struct Modifying /* want to be ~Copyable */ {
+
+          public private (set) var $_readIdentifiers: Set<String> = .init()
+          public private (set) var $_modifiedIdentifiers: Set<String> = .init()
+
+          private let pointer: UnsafeMutablePointer<ModifyingTarget>
+
+          init(pointer: UnsafeMutablePointer<ModifyingTarget>) {
+            self.pointer = pointer
+          }
+
+          public var constant_has_initial_value: Int  {
+          mutating _read {
+            $_readIdentifiers.insert("constant_has_initial_value")
+            yield pointer.pointee.constant_has_initial_value
+          }
+          _modify {
+            $_modifiedIdentifiers.insert("constant_has_initial_value")
+            yield &pointer.pointee.constant_has_initial_value
+          }
+        }
+        }
+      }
+      """
     }
-  }
-
-  @discardableResult
-  public static func read(source: Self, reader: (Modifying) throws -> Void) rethrows -> ReadResult {
-    // FIXME: avoid copying
-    var reading = source
-
-    return try withUnsafeMutablePointer(to: &reading) { pointer in
-      let modifying = Modifying(pointer: pointer)
-      try reader(modifying)
-      return ReadResult(
-        readIdentifiers: modifying.$_readIdentifiers
-      )
-    }
-  }
-
-  public struct Modifying /* want to be ~Copyable */ {
-
-    public private (set) var $_readIdentifiers: Set<String> = .init()
-    public private (set) var $_modifiedIdentifiers: Set<String> = .init()
-
-    private let pointer: UnsafeMutablePointer<ModifyingTarget>
-
-    init(pointer: UnsafeMutablePointer<ModifyingTarget>) {
-      self.pointer = pointer
-    }
-
-    public var constant_has_initial_value: Int  {
-    mutating _read {
-      $_readIdentifiers.insert("constant_has_initial_value")
-      yield pointer.pointee.constant_has_initial_value
-    }
-    _modify {
-      $_modifiedIdentifiers.insert("constant_has_initial_value")
-      yield &pointer.pointee.constant_has_initial_value
-    }
-  }
-  }
-}
-"""#,
-      macros: ["Detecting": WriterMacro.self]
-    )
 
   }
   func test_getter() {
 
-    assertMacroExpansion(
+    assertMacro {
       #"""
-      @Writing
+      @Detecting
+      struct MyState {
+      
+        var computed_read_only: Int {
+          constant_has_initial_value
+        }
+      
+        var computed_read_only2: Int {
+          get {
+            constant_has_initial_value
+          }
+        }
+      
+        var computed_readwrite: String {
+          get {
+            variable_no_initial_value
+          }
+          set {
+            variable_no_initial_value = newValue
+          }
+        }
+      }
+      """#
+    } expansion: {
+      """
       struct MyState {
 
         var computed_read_only: Int {
@@ -189,103 +223,78 @@ extension MyState: DetectingType {
           }
         }
       }
-      """#,
-      expandedSource: #"""
-struct MyState {
 
-  var computed_read_only: Int {
-    constant_has_initial_value
-  }
+      extension MyState: DetectingType {
 
-  var computed_read_only2: Int {
-    get {
-      constant_has_initial_value
-    }
-  }
+        typealias ModifyingTarget = Self
 
-  var computed_readwrite: String {
-    get {
-      variable_no_initial_value
-    }
-    set {
-      variable_no_initial_value = newValue
-    }
-  }
-}
+        @discardableResult
+        public static func modify(source: inout Self, modifier: (inout Modifying) throws -> Void) rethrows -> ModifyingResult {
 
-extension MyState: DetectingType {
-
-  typealias ModifyingTarget = Self
-
-  @discardableResult
-  public static func modify(source: inout Self, modifier: (inout Modifying) throws -> Void) rethrows -> ModifyingResult {
-
-    try withUnsafeMutablePointer(to: &source) { pointer in
-      var modifying = Modifying(pointer: pointer)
-      try modifier(&modifying)
-      return ModifyingResult(
-        readIdentifiers: modifying.$_readIdentifiers,
-        modifiedIdentifiers: modifying.$_modifiedIdentifiers
-      )
-    }
-  }
-
-  @discardableResult
-  public static func read(source: Self, reader: (Modifying) throws -> Void) rethrows -> ReadResult {
-    // FIXME: avoid copying
-    var reading = source
-
-    return try withUnsafeMutablePointer(to: &reading) { pointer in
-      let modifying = Modifying(pointer: pointer)
-      try reader(modifying)
-      return ReadResult(
-        readIdentifiers: modifying.$_readIdentifiers
-      )
-    }
-  }
-
-  public struct Modifying /* want to be ~Copyable */ {
-
-    public private (set) var $_readIdentifiers: Set<String> = .init()
-    public private (set) var $_modifiedIdentifiers: Set<String> = .init()
-
-    private let pointer: UnsafeMutablePointer<ModifyingTarget>
-
-    init(pointer: UnsafeMutablePointer<ModifyingTarget>) {
-      self.pointer = pointer
-    }
-
-    public var computed_read_only: Int
-  {
-    mutating get {
-      constant_has_initial_value
-  }
-  }
-
-  public var computed_read_only2: Int
-  {
-    mutating
-        get {
-          constant_has_initial_value
+          try withUnsafeMutablePointer(to: &source) { pointer in
+            var modifying = Modifying(pointer: pointer)
+            try modifier(&modifying)
+            return ModifyingResult(
+              readIdentifiers: modifying.$_readIdentifiers,
+              modifiedIdentifiers: modifying.$_modifiedIdentifiers
+            )
+          }
         }
-  }
 
-  public var computed_readwrite: String
-  {
-    mutating
-        get {
-          variable_no_initial_value
-        }
-        set {
-          variable_no_initial_value = newValue
-        }
-  }
-  }
-}
-"""#,
-      macros: ["Writing": WriterMacro.self]
-    )
+        @discardableResult
+        public static func read(source: Self, reader: (Modifying) throws -> Void) rethrows -> ReadResult {
+          // FIXME: avoid copying
+          var reading = source
 
+          return try withUnsafeMutablePointer(to: &reading) { pointer in
+            let modifying = Modifying(pointer: pointer)
+            try reader(modifying)
+            return ReadResult(
+              readIdentifiers: modifying.$_readIdentifiers
+            )
+          }
+        }
+
+        public struct Modifying /* want to be ~Copyable */ {
+
+          public private (set) var $_readIdentifiers: Set<String> = .init()
+          public private (set) var $_modifiedIdentifiers: Set<String> = .init()
+
+          private let pointer: UnsafeMutablePointer<ModifyingTarget>
+
+          init(pointer: UnsafeMutablePointer<ModifyingTarget>) {
+            self.pointer = pointer
+          }
+
+          public var computed_read_only: Int
+        {
+          mutating get {
+            constant_has_initial_value
+        }
+        }
+
+        public var computed_read_only2: Int
+        {
+          mutating
+              get {
+                constant_has_initial_value
+              }
+        }
+
+        public var computed_readwrite: String
+        {
+          mutating
+              get {
+                variable_no_initial_value
+              }
+              set {
+                variable_no_initial_value = newValue
+              }
+        }
+        }
+      }
+      """
+    }
   }
 }
 
